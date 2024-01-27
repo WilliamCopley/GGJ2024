19b4ce634b948fcd99fa709ed2036c4f9b5f0ebb
emily <crabarc@gmail.com>
2024-01-27 16:55:48 +1030

    Added basic building components

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -7,10 +7,17 @@ public class Interactable : MonoBehaviour
 {
     private bool toggleState = false;
     [SerializeField] private UnityEvent<bool> interacted;
-    
+    [SerializeField]
+    private Animator changingAnimator;
+    [SerializeField]
+    private string animatorField = "interactionToggle";
     public void doInteract()
     {
         toggleState = !toggleState;
         interacted.Invoke(toggleState);
+        if(changingAnimator != null)
+        {
+            changingAnimator.SetBool(animatorField, toggleState);
+        }
     }
 }
