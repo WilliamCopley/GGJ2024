2d50a07811e04b7842b4d2159b84bd90c86a9a3e
Ryan Swiatnik <swiryan@gmail.com>
2024-01-27 14:52:55 +1030

    Added credits screen.

---
+++ Assets/Scripts/MainMenu/Credits.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] private MainMenu mainMenu;
+    [SerializeField] private RectTransform creditsList;
+    [SerializeField] private float scrollSpeed = 100;
+    private float startYPos;
+    private float yPos; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yPos = creditsList.localPosition.y;
+        startYPos = creditsList.localPosition.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (yPos > startYPos * -1)
+        {
+            yPos -= scrollSpeed * Time.deltaTime;
+            creditsList.transform.localPosition = new Vector3(0, yPos, 0);
+        } else
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -23,7 +24,7 @@ public class MainMenu : MonoBehaviour
 
     public void Credits()
     {
-        Debug.Log("Pressed Credits");
+        SceneManager.LoadScene("Credits");
     }
 
     public void Quit()
